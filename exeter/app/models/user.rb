4fbfa373d064c7063ffb05d22790bf4da2eb37a3
Shaun Thium <shaun.thium@gmail.com>
2015-10-29 01:33:52 +0800

    Create working signup form.

--- exeter/app/models/user.rb
+++ exeter/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  before_save { self.email = email.downcase }
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
--- exeter/test/models/user_test.rb
+++ exeter/test/models/user_test.rb
@@ -4,4 +4,29 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+      @user = User.new(name: "Example", email: "john@example.com", password: "123", password_confirmation: "123")
+  end
+
+  test "user should be valid" do
+      assert @user.valid?
+  end
+
+  test "user's name should not be empty" do
+      @user.name = '    '
+      assert_not @user.valid?
+  end
+
+  test "user's email should not be empty" do
+      @user.email = '    '
+      assert_not @user.valid?
+  end
+
+  test "user's email should be unique, regardless of case" do
+      duplicate_user = @user.dup
+      @user.save
+      duplicate_user.email = @user.email.upcase
+      assert_not duplicate_user.valid?
+  end
 end
