aa69f73b9211b8f21861fa2968e966828b0c118d
Shaun Thium <shaun.thium@gmail.com>
2015-11-03 13:13:56 +0800

    Add error messages partial. Add form control to text inputs.

--- exeter/app/models/user.rb
+++ exeter/app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   before_save { self.email = email.downcase }
   validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
