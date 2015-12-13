f3d46c3d1256157c5c6015728d3e81ba922a7037
Shaun Thium <shaun.thium@gmail.com>
2015-12-13 19:30:12 +0800

    Refactor User's show view and Application menu bar for aesthetic appeal.

--- exeter/config/routes.rb
+++ exeter/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     post 'login' => 'sessions#create'
     delete 'logout' => 'sessions#destroy'
 
-    resources :users do
+    resources :users, except: :index do
         get :friends
         resources :groups
     end
