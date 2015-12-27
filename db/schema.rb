881a7029fc1df2e472fddd7b85ed275b0f413215
Shaun Thium <shaun.thium@gmail.com>
2015-12-27 12:34:44 +0800

    Remove unused 'admin' column from Users; Adminships model already takes care of admin. Other minor edits.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222105407) do
+ActiveRecord::Schema.define(version: 20151226144414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 20151222105407) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.boolean  "admin"
     t.string   "slug"
     t.string   "remember_digest"
   end
