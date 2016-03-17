5925b860f9fcd45d90e37ee042d91e9119867d08
Shaun Thium <shaun.thium@gmail.com>
2016-03-18 02:20:45 +0800

    Create PictureUploader and configure for resizing with ImageMagick.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230073426) do
+ActiveRecord::Schema.define(version: 20160317175654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 20151230073426) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
